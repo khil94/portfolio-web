032a9bb04c4a1ed17c036a4a5e3212d3959e97b0
khil94 <zidru94@gmail.com>
2024-03-16 00:24:45 +0900

    [feat] : 포트폴리오 섹션들 미리 정의

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 import SidebarLayout from "@/layouts/SidebarLayout";
+import AboutSection from "@/sections/AboutSection";
+import ContactSection from "@/sections/ContactSection";
 import MainSection from "@/sections/MainSection";
-import TestSection from "@/sections/TestSection";
+import ProjectSection from "@/sections/ProjectSection";
+import SkillSection from "@/sections/SkillSection";
 
 export default function Home() {
   return (
     <main>
       <SidebarLayout>
         <MainSection />
-        <TestSection />
+        <AboutSection />
+        <ProjectSection />
+        <SkillSection />
+        <ContactSection />
       </SidebarLayout>
     </main>
   );
---
+++ src/sections/AboutSection.tsx
@@ -0,0 +1,13 @@
+export default function AboutSection() {
+  return (
+    <section
+      id="section0"
+      className="max-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:pt-32 flex min-h-screen items-center"
+    >
+      <h2 className="hidden">About</h2>
+      <h1 className="mx-auto max-w-4xl text-5xl font-medium tracking-tight h-auto text-slate-500">
+        FrontEnd 개발자 김효일의 포트폴리오입니다.
+      </h1>
+    </section>
+  );
+}
