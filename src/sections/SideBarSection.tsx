6235e2f84e62a9b4893f5a37ae9cd8a0e25c8417
khil94 <zidru94@gmail.com>
2024-03-17 13:49:33 +0900

    [feat] : 사이드바를 눌러서 해당 섹션으로 이동시 부드럽게 이동

--- src/sections/SideBarSection.tsx
+++ src/sections/SideBarSection.tsx
@@ -6,18 +6,28 @@ interface IProp {
 }
 
 export default function SideBarSection({ headlings, target }: IProp) {
-  console.log(headlings);
   return (
     <aside className=" max-sm:hidden bg-white bg-opacity-50 fixed top-0 left-0 z-40 w-64 h-screen p-5">
       <ul>
         {headlings.map((h, i) => {
           return (
             <li
+              key={"href_to_" + h.id}
               className={` flex items-center h-10 list-none transition-all duration-300 ${
                 target === i ? "font-bold text-red-500 text-lg" : ""
               }`}
             >
-              <a href={`#${h.id}`}>{h.title}</a>
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  document.querySelector(`#${h.id}`)?.scrollIntoView({
+                    behavior: "smooth",
+                  });
+                }}
+                href={`#${h.id}`}
+              >
+                {h.title}
+              </a>
             </li>
           );
         })}
