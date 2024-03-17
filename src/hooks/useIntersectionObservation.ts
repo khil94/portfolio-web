cd421ee5256af31b73d1b3da8503da94d1887402
khil94 <zidru94@gmail.com>
2024-03-17 18:15:41 +0900

    [feat] : IntersectionObserver 수정

--- src/hooks/useIntersectionObservation.ts
+++ src/hooks/useIntersectionObservation.ts
@@ -3,16 +3,21 @@ import { Dispatch, SetStateAction, useEffect, useState } from "react";
 export function useIntersectionObservation(
   setter: Dispatch<SetStateAction<string>>
 ) {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const target = entry.target as HTMLElement;
-        if (target.dataset.id) {
-          setter(target.dataset.id);
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const target = entry.target as HTMLElement;
+          if (target.id) {
+            setter(target.id);
+          }
         }
-      }
-    });
-  });
+      });
+    },
+    {
+      threshold: 0.7,
+    }
+  );
 
   return observer;
 }
@@ -22,7 +27,7 @@ export function useScrollSpy() {
 
   useEffect(() => {
     const observer = useIntersectionObservation(setCurrentId);
-    const headingEls = Array.from(document.querySelectorAll("h1"));
+    const headingEls = Array.from(document.querySelectorAll("section"));
     headingEls.forEach((v) => {
       console.log(v);
       observer.observe(v);
