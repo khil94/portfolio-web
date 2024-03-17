d6d16aa02146444f96457e747428543298c88ce8
khil94 <zidru94@gmail.com>
2024-03-17 14:28:40 +0900

    [feat] : About section 내용 임시 기입

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -2,12 +2,19 @@ export default function AboutSection() {
   return (
     <section
       id="section1"
-      className="max-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:pt-32 flex min-h-screen items-center"
+      className="max-auto px-4 sm:px-6 lg:px-8 lg:pt-32 flex min-h-screen items-center"
     >
       <h2 className="hidden">About</h2>
-      <h1 className="mx-auto max-w-4xl text-5xl font-medium tracking-tight h-auto text-slate-500">
-        FrontEnd 개발자 김효일의 포트폴리오입니다.
-      </h1>
+      <h1 className="mx-auto text-5xl text-slate-500">About Me</h1>
+      <p className=" p-5">
+        안녕하세요! 커뮤니케이션을 선호하는 개발자입니다. React를 주력으로
+        사용하는 주니어 프론트엔드 개발자로, 1년간의 현업에서의 개발 경험이
+        있습니다. 코딩 컨벤션을 맞추기 위한 회의나 코드 리뷰, 회의를 통한
+        의견교환등 서로간의 커뮤니케이션이 자주 일어나고 이를 통해 개발 효율이
+        올라가는 것을 좋아합니다. 사용자와 가장 직접적으로 상호작용하는
+        프론트엔드를 담당하고 있는 만큼, 다양한 의견을 좋아합니다. 특히 비
+        개발자 분들의 의견이 너무 소중합니다!
+      </p>
     </section>
   );
 }
