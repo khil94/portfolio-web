2505a0502d28b7f88c704a1b9d6157a2dbe2f69a
khil94 <zidru94@gmail.com>
2024-03-21 20:04:58 +0900

    [feat] : 화면이 작으면 화살표를 통해 다른 박스를 보여주는 기능 추가

--- src/sections/SkillSection.tsx
+++ src/sections/SkillSection.tsx
@@ -1,6 +1,6 @@
 import { SKILL_LIST } from "@/constants/constants";
 import { IImageCompProp } from "@/types/type";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface IProp {
   target: boolean;
@@ -8,14 +8,33 @@ interface IProp {
 
 export default function SkillSection({ target }: IProp) {
   const [idx, setIdx] = useState(0);
+  const [width, setWidth] = useState<number>();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", handleResize);
+      setWidth(window.screen.width);
+      console.log(window.screen.width, width);
+    }
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   function ImgComponent({ src, text }: IImageCompProp) {
     return (
       <div className="flex items-center w-24 m-8 relative group">
-        <img
-          src={`/${src}.png`}
-          width={96}
-          className="mb-2 group-hover:scale-110 group-hover:duration-100 group-hover:blur-sm"
-        />
+        <div className=" w-24 h-24 flex items-center">
+          <img
+            src={`/${src}.png`}
+            width={96}
+            className=" group-hover:scale-110 group-hover:duration-100 group-hover:blur-sm"
+          />
+        </div>
         <span
           className=" hidden group-hover:inline group-hover:visible 
         group-hover:absolute group-hover:left-1/2 group-hover:top-1/2 
@@ -29,24 +48,47 @@ export default function SkillSection({ target }: IProp) {
   }
 
   function SkillBox() {
+    if (width && width <= 1440) {
+      return (
+        <>
+          <div className="relative mx-3 h-full border-2 rounded-lg border-black p-8 grid grid-cols-2">
+            <span className="absolute font-bold text-lg top-0 left-1/2 translate-y-4 translate-x-[-50%]">
+              {SKILL_LIST[idx].title}
+            </span>
+            <img
+              src="/left-arrow.png"
+              width={32}
+              onClick={() => setIdx(idx - 1 > 0 ? idx - 1 : 0)}
+              className="arrow absolute left-0 top-1/2 translate-y-[-50%] mb-2 active:opacity-40"
+            />
+            <img
+              src="/right-arrow.png"
+              width={32}
+              onClick={() =>
+                setIdx(
+                  idx + 1 < SKILL_LIST.length - 1
+                    ? idx + 1
+                    : SKILL_LIST.length - 1
+                )
+              }
+              className="arrow absolute right-0 top-1/2 translate-y-[-50%] active:opacity-40"
+            />
+            {SKILL_LIST[idx].items.map((v) => {
+              return <ImgComponent key={v.src} src={v.src} text={v.text} />;
+            })}
+          </div>
+        </>
+      );
+    }
+
     return (
       <>
         {SKILL_LIST.map((v) => {
           return (
-            <div className="relative h-full border-2 rounded-lg border-black p-8 grid grid-cols-2">
-              <span className="absolute font-bold text-xl top-0 left-1/2 translate-y-4 translate-x-[-50%]">
+            <div className="relative mx-3 h-full border-2 rounded-lg border-black p-8 grid grid-cols-2">
+              <span className="absolute font-bold text-lg top-0 left-1/2 translate-y-4 translate-x-[-50%]">
                 {v.title}
               </span>
-              {/* <img
-    src="/left-arrow.png"
-    width={32}
-    className="absolute left-0 top-1/2 translate-y-[-50%]"
-  />
-  <img
-    src="/right-arrow.png"
-    width={32}
-    className="absolute right-0 top-1/2 translate-y-[-50%]"
-  /> */}
               {v.items.map((v) => {
                 return <ImgComponent key={v.src} src={v.src} text={v.text} />;
               })}
@@ -60,7 +102,7 @@ export default function SkillSection({ target }: IProp) {
   return (
     <section
       id="section2"
-      className="max-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:py-32 flex min-h-screen items-center"
+      className="max-auto px-4 mob:py-8 mob:px-0 sm:py-16 sm:px-6 lg:px-8 lg:py-32 flex min-h-screen items-center justify-center"
     >
       <h2 className="hidden">Skill</h2>
       {/* <h1>Skill</h1> */}
