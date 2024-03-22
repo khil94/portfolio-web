fb036184a442e685a2a7d088e8817c0568798bf5
khil94 <zidru94@gmail.com>
2024-03-22 20:35:57 +0900

    [feat] : Carousel 애니메이션 효과 추가

--- src/sections/SkillSection.tsx
+++ src/sections/SkillSection.tsx
@@ -1,13 +1,14 @@
 import { SKILL_LIST } from "@/constants/constants";
 import { IImageCompProp } from "@/types/type";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface IProp {
   target: boolean;
 }
 
 export default function SkillSection({ target }: IProp) {
-  const [idx, setIdx] = useState(0);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const idx = useRef(0);
   const [width, setWidth] = useState<number>(0);
 
   useEffect(() => {
@@ -17,13 +18,21 @@ export default function SkillSection({ target }: IProp) {
     if (typeof window !== "undefined") {
       window.addEventListener("resize", handleResize);
       setWidth(window.screen.width);
-      console.log(window.screen.width, width);
     }
 
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  function translator(target: number) {
+    if (wrapperRef.current) {
+      wrapperRef.current.style.setProperty(
+        "transform",
+        `translateX(-${target * 320}px)`
+      );
+    }
+  }
 
   function ImgComponent({ src, text }: IImageCompProp) {
     return (
@@ -32,7 +41,7 @@ export default function SkillSection({ target }: IProp) {
           <img
             src={`/${src}.png`}
             width={96}
-            className=" group-hover:scale-110 group-hover:duration-100 group-hover:blur-sm"
+            className=" group-hover:scale-110 transform group-hover:duration-100 group-hover:blur-sm"
           />
         </div>
         <span
@@ -47,36 +56,63 @@ export default function SkillSection({ target }: IProp) {
     );
   }
 
+  function SkillComponentList() {
+    return (
+      <>
+        {SKILL_LIST.map((v) => {
+          return (
+            <div
+              className={`relative min-w-80 border-2 rounded-lg border-black py-8 grid grid-cols-2`}
+            >
+              <span className="absolute font-bold text-lg top-0 left-1/2 translate-y-4 translate-x-[-50%]">
+                {v.title}
+              </span>
+              {v.items.map((v) => {
+                return <ImgComponent key={v.src} src={v.src} text={v.text} />;
+              })}
+            </div>
+          );
+        })}
+      </>
+    );
+  }
+
   function SkillBox() {
     if (width) {
       if (width <= 1440) {
         return (
           <>
-            <div className="relative mx-3 h-full border-2 rounded-lg border-black p-8 grid grid-cols-2">
-              <span className="absolute font-bold text-lg top-0 left-1/2 translate-y-4 translate-x-[-50%]">
-                {SKILL_LIST[idx].title}
-              </span>
+            <div className={`relative w-80 overflow-hidden`}>
+              <div
+                ref={wrapperRef}
+                className={`slide-wrapper h-full flex duration-1000`}
+              >
+                <SkillComponentList />
+              </div>
               <img
                 src="/left-arrow.png"
                 width={32}
-                onClick={() => setIdx(idx - 1 > 0 ? idx - 1 : 0)}
+                onClick={() => {
+                  const target =
+                    idx.current - 1 >= 0
+                      ? idx.current - 1
+                      : SKILL_LIST.length - 1;
+                  translator(target);
+                  idx.current = target;
+                }}
                 className="arrow absolute left-0 top-1/2 translate-y-[-50%] mb-2 active:opacity-40"
               />
               <img
                 src="/right-arrow.png"
                 width={32}
-                onClick={() =>
-                  setIdx(
-                    idx + 1 < SKILL_LIST.length - 1
-                      ? idx + 1
-                      : SKILL_LIST.length - 1
-                  )
-                }
+                onClick={() => {
+                  const target =
+                    idx.current + 1 < SKILL_LIST.length ? idx.current + 1 : 0;
+                  translator(target);
+                  idx.current = target;
+                }}
                 className="arrow absolute right-0 top-1/2 translate-y-[-50%] active:opacity-40"
               />
-              {SKILL_LIST[idx].items.map((v) => {
-                return <ImgComponent key={v.src} src={v.src} text={v.text} />;
-              })}
             </div>
           </>
         );
