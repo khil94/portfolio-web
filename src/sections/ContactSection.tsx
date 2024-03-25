90e2934a9f6f16a31c88af4c16be5ff582e7733c
khil94 <zidru94@gmail.com>
2024-03-25 17:47:40 +0900

    [feat] : svg to react component

--- src/sections/ContactSection.tsx
+++ src/sections/ContactSection.tsx
@@ -1,13 +1,17 @@
+import Email from "../../public/email.svg";
+import Github from "../../public/github.svg";
+
 export default function ContactSection() {
   return (
     <section
       id="section4"
-      className="max-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:pt-32 flex min-h-screen items-center"
+      className="max-auto bg-slate-800 px-4 sm:px-6 lg:px-8 lg:pt-32 flex items-center"
     >
       <h2 className="hidden">Contact</h2>
-      <h1 className="mx-auto max-w-4xl text-5xl font-medium tracking-tight h-auto text-slate-500">
-        FrontEnd 개발자 김효일의 포트폴리오입니다.
-      </h1>
+      <div>
+        <Github width={96} height={96} fill={"white"} />
+        <Email width={96} height={96} fill={"white"} />
+      </div>
     </section>
   );
 }
