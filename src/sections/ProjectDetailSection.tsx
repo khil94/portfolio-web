02598f2f6712b5e4b324cf99c638ea9435d107c2
khil94 <zidru94@gmail.com>
2024-03-25 16:49:09 +0900

    [style] : ProjectSection border color 추가

--- src/sections/ProjectDetailSection.tsx
+++ src/sections/ProjectDetailSection.tsx
@@ -9,11 +9,11 @@ export default function ProjectDetailSection({ data, isTarget }: IProp) {
   return (
     <div
       key={data.name + "-wrapper"}
-      className={`flex even:justify-end w-full mb-12 ${
+      className={`flex even:justify-end w-full mb-12 group ${
         isTarget ? "animate-appear even:animate-appearRight" : "opacity-0"
       }`}
     >
-      <div className="border-2 border-black p-8 rounded-lg ">
+      <div className="border-2 group-even:border-red-300 shadow-xl group-odd:border-indigo-400 p-8 rounded-lg ">
         <h3 id={data.name} className=" font-semibold mb-3">
           {data.name}
         </h3>
