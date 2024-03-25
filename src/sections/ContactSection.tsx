65d773be68748107e3f30f5a1c6c1dd45c9ea43c
khil94 <zidru94@gmail.com>
2024-03-25 20:39:39 +0900

    [feat] : ContactSection

--- src/sections/ContactSection.tsx
+++ src/sections/ContactSection.tsx
@@ -3,14 +3,15 @@ import Github from "../../public/github.svg";
 
 export default function ContactSection() {
   return (
-    <section
-      id="section4"
-      className="max-auto bg-slate-800 px-4 sm:px-6 lg:px-8 lg:pt-32 flex items-center"
-    >
+    <section id="section4" className="max-auto bg-slate-800 flex items-center">
       <h2 className="hidden">Contact</h2>
-      <div>
-        <Github width={96} height={96} fill={"white"} />
-        <Email width={96} height={96} fill={"white"} />
+      <div className="flex w-full h-14 items-center justify-center ">
+        <a href="https://github.com/khil94">
+          <Github width={48} height={48} fill={"white"} />
+        </a>
+        <a href="mailto: khilkhil@naver.com">
+          <Email width={48} height={48} fill={"white"} />
+        </a>
       </div>
     </section>
   );
