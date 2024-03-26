7e2dd80ccb59c6f27ecbdeab74035be7775da40f
khil94 <zidru94@gmail.com>
2024-03-26 15:27:43 +0900

    [style] : tag 컴포넌트 border width 조절

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -4,7 +4,7 @@ interface IProp {
 
 export default function Tag({ content }: IProp) {
   return (
-    <div className=" inline-block bg-orange-500 rounded-3xl p-2 cursor-default text-white">
+    <div className=" inline-block border-white border-2 bg-orange-500 rounded-3xl p-2 cursor-default text-white">
       #{content}
     </div>
   );
